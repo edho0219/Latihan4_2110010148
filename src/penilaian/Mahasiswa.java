7308b9fa188e6cf5199cd9817701b646b8feb90b
edho0219 <edho0219@gmail.com>
2023-06-13 09:30:41 +0700

    Membuat program latihan 4

---
+++ src/penilaian/Mahasiswa.java
@@ -0,0 +1,29 @@
+package penilaian;
+
+public class Mahasiswa {
+    private String nama;
+    private String npm;
+    private int uts, uas, tugas;
+    
+    public Mahasiswa (String nama, String npm){
+        this.nama = nama;
+        this.npm = npm;
+    }
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+    
+    
+}
---
+++ src/penilaian/NilaiAkhir.java
@@ -0,0 +1,12 @@
+package penilaian;
+
+public class NilaiAkhir extends Mahasiswa {
+   
+    public NilaiAkhir(String nama, String npm) {
+        super(nama, npm);
+    }
+  public Mahasiswa (String nama, String npm){
+        this.nama = nama;
+        this.npm = npm;
+    }
+}
